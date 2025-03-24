082d3dffa0c2fb67b150e369a0da942dbe8b1294
J-Mowbray <j.mowbray96@gmail.com>
2025-03-24 15:19:23 +0000

    Created View Model for Category

---
+++ CustomerWeb/Models/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomerWeb.Models;
+
+public class CategoryViewModel
+{
+    public int CategoryId { get; set; }
+    public string? CategoryName { get; set; }
+    public int ProductCount { get; set; }
+}
